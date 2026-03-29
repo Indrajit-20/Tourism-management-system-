3d300a85591fea47911d22bc0a500eb24258996e
Harsh Rana <ranaharsh2508@gmail.com>
2026-03-29 16:50:13 +0530

    tourbookingController.js

--- backend/routes/tourbookingRoutes.js
+++ backend/routes/tourbookingRoutes.js
@@ -6,7 +6,6 @@ const {
   getPackageBookedSeats,
   getAllPackageBookings,
   updatePackageBookingStatus,
-  updateMyPackageBooking,
   getMyBookings,
 } = require("../controllers/tourbookingController");
 const { authMiddleware, isadmin } = require("../middleware/authmiddleware");
@@ -15,7 +14,6 @@ const { authMiddleware, isadmin } = require("../middleware/authmiddleware");
 router.get("/package-seats/:package_id", getPackageBookedSeats);
 router.post("/book", authMiddleware, packageBooking);
 router.get("/my-bookings", authMiddleware, getMyBookings);
-router.put("/my-bookings/:id", authMiddleware, updateMyPackageBooking);
 
 // Admin routes
 router.get("/all", authMiddleware, isadmin, getAllPackageBookings);
